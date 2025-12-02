decb10345b3e1a1459c75e36c0ce74ab73241ada
himanshudutta0 <dutthimsun@gmail.com>
2025-12-02 16:28:43 +0530

    new sensor modules~

--- aqua_sense_agent/main.py
+++ aqua_sense_agent/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "FastAPI working!"}
+
+
+import requests
+
+url = "http://127.0.0.1:8000/primary_sensors/stream"
+with requests.get(url, stream=True) as r:
+    for line in r.iter_lines():
+        if line.startswith(b"data:"):
+            payload = line.replace(b"data:", b"").strip()
+            print(payload)
